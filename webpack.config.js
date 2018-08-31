e04a1276ac5785bffe781d2123d8cb5d820fbac0
Kirill Selyukov <kirill.selyukov@dfsco.com>
2018-08-31 19:44:41 +0300

    Add babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,20 @@ module.exports = {
     devtool: 'cheap-module-eval-source-map',
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname,'dist'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
         publicPath: ''
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js$/,
+                loader:'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
     }
 };
